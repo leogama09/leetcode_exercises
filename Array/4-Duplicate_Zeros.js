c87de1f88d2ec51fe534f755426616b9e97cd4cb
Leonardo Gama <leogama09@gmail.com>
2023-05-31 12:14:19 -0300

    Create 4-Duplicate_Zeros.js

---
+++ Array/4-Duplicate_Zeros.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    const N = arr.length
+  for (let i = 0; i < N - 1; i++) {
+    if (arr[i] === 0) {
+      let prev = arr[i + 1]
+      for (let j = i + 2; j < N; j++) {
+        let temp = arr[j]
+        arr[j] = prev
+        prev = temp
+      }
+      arr[++i] = 0
+    }
+  }
+};
